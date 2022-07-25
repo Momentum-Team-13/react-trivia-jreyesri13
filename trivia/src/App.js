75c1263d1aec48289888eeeb9912fd74e03608ba
Jose Reyes <jreyesri@cbu.edu>
2022-07-25 13:30:07 -0400

    Need to finish questions page

--- trivia/src/App.js
+++ trivia/src/App.js
@@ -1,54 +1,11 @@
 import './App.css';
-// import QuizEx from "./quizTut";
-// import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useState, useEffect } from 'react';
+import QuizCategories from './Categories'
 
 function App() {
 
-  // const [resourceType, setResourceType] = useState('20');
-
-  // `https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple`
-  // `https://jsonplaceholder.typicode.com/${resourceType}/10`
-  // 20, 21, 22
-
-  // useEffect(() => {
-  //   fetch(`https://opentdb.com/api.php?amount=10&category=20&difficulty=easy&type=multiple`)
-  //     .then(response => response.json())
-  //     .then(json => console.log(json))
-  // },
-  //   [resourceType])
-
-
-  const [quizData, setQuizData] = useState([])
-
-  useEffect(() => {
-    axios
-      .get(`https://opentdb.com/api.php?amount=10&category=20&difficulty=easy&type=multiple`)
-      .then((res) => {
-        console.log(res.data.results)
-        setQuizData(res.data.results)
-      })
-  }, [])
-
-  const quizArray = quizData.map((data, index) => {
-    return(
-      <p>{data.question}</p>
-    )
-
-  })
-
-
   return (
     <>
-      {/* <QuizEx /> */}
-      {/* <div>
-        <button onClick={() => setResourceType('20')}>Mythology</button>
-        <button onClick={() => setResourceType('21')}>Sports</button>
-        <button onClick={() => setResourceType('22')}>Geography</button>
-      </div>
-      <h1>{resourceType}</h1> */}
-      {quizArray}
+      <QuizCategories/>
     </>
 
   );
---
+++ trivia/src/Categories.js
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import QuizQuestions from './Questions'
+
+export default function QuizCategories() {
+
+    const [categories, setCategories] = useState([])
+
+    const [selectedCategory, setSelectedCategory] = useState(null)
+
+    const [categoryURL, setCategoryURL] = useState()
+
+
+    // Calls the trivia api
+    useEffect(() => {
+        axios
+            .get(`https://opentdb.com/api_category.php`)
+            .then((res) => {
+                console.log(res.data.trivia_categories)
+                setCategories(res.data.trivia_categories)
+            })
+    }, [])
+
+
+    const handleSelectedCategory = (selectedCat) => {
+        console.log(`ID: ${selectedCat.id}, Category: ${selectedCat.name}, `)
+        setSelectedCategory(selectedCat)
+        customURL(selectedCat)
+    }
+
+
+    const customURL = (selectedCategory) => {
+        setCategoryURL(`https://opentdb.com/api.php?amount=10&category=${selectedCategory.id}&type=multiple`)
+    }
+
+
+    return (
+        <>
+            {selectedCategory ? (
+                <>
+                    {/* <Questions categoryID = {categoryURL} /> */}
+                    {categoryURL}
+                </>
+            ) : (
+                <>
+                    <h1>Trivia Game</h1>
+                    <h2>Pick a Category</h2>
+                    {categories.map((category) => (
+                        <button onClick={() => handleSelectedCategory(category)}>{category.name}</button>
+                    ))}
+                </>
+            )
+            }
+
+        </>
+    )
+}
