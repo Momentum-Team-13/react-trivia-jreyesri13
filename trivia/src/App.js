91daceb5588c157a4dfcfb373754acd31a0d732d
Jose Reyes <jreyesri@cbu.edu>
2022-07-25 02:31:37 -0400

    Worked on app

--- trivia/src/App.js
+++ trivia/src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+// import QuizEx from "./quizTut";
+// import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  // const [resourceType, setResourceType] = useState('20');
+
+  // `https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple`
+  // `https://jsonplaceholder.typicode.com/${resourceType}/10`
+  // 20, 21, 22
+
+  // useEffect(() => {
+  //   fetch(`https://opentdb.com/api.php?amount=10&category=20&difficulty=easy&type=multiple`)
+  //     .then(response => response.json())
+  //     .then(json => console.log(json))
+  // },
+  //   [resourceType])
+
+
+  const [quizData, setQuizData] = useState([])
+
+  useEffect(() => {
+    axios
+      .get(`https://opentdb.com/api.php?amount=10&category=20&difficulty=easy&type=multiple`)
+      .then((res) => {
+        console.log(res.data.results)
+        setQuizData(res.data.results)
+      })
+  }, [])
+
+  const quizArray = quizData.map((data, index) => {
+    return(
+      <p>{data.question}</p>
+    )
+
+  })
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <QuizEx /> */}
+      {/* <div>
+        <button onClick={() => setResourceType('20')}>Mythology</button>
+        <button onClick={() => setResourceType('21')}>Sports</button>
+        <button onClick={() => setResourceType('22')}>Geography</button>
+      </div>
+      <h1>{resourceType}</h1> */}
+      {quizArray}
+    </>
+
   );
 }
 
